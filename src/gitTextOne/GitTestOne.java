2b6efd54de4d55faea04fc91c9eb52009e86abd5
yxd_gbl <Yxd777@yxd_gbl.com>
2021-05-03 17:37:42 +0800

    GitHub commit test

---
+++ src/gitTextOne/GitTestOne.java
@@ -0,0 +1,10 @@
+package gitTextOne;
+
+public class GitTestOne {
+    public static void main(String[] args) {
+        int a=1;
+        int b=2;
+        int num=a+b;
+        System.out.println("a+b="+num);
+    }
+}
